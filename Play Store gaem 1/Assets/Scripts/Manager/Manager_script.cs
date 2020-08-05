36ce6d6ade045b12d634a31262b7cd5054a702bd
Daxxboii <64298070+Daxxboii@users.noreply.github.com>
2020-08-05 12:01:07 +0530

    Basic

---
+++ Play Store gaem 1/Assets/Scripts/Manager/Manager_script.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager_script : MonoBehaviour
+{
+    public Material emission;
+  
+   
+    public void Update()
+    {
+        emission.SetColor("_EmissionColor", Random.ColorHSV(0f, 360f,98f,100f,98f,100f));
+  
+    }
+}
