61cc67bfb217570fd3d70942479f4dc8236d4915
Daxxboii <64298070+Daxxboii@users.noreply.github.com>
2020-08-08 18:16:08 +0530

    Player setup

--- Play Store gaem 1/Assets/Scripts/Manager/Manager_spawner.cs
+++ Play Store gaem 1/Assets/Scripts/Manager/Manager_spawner.cs
@@ -2,23 +2,28 @@
 
 public class Manager_spawner : MonoBehaviour
 {
-    public float offset = 2f;
+    private float offset = 2f;
+    private float _offset;
     public float Render_distance = 100f;
-    public GameObject scene_base;
+    public Transform scene_base;
     public GameObject base_model;
 
     private Vector3 spawn_location;
 
     private void Awake()
     {
-        
+        _offset = offset;
       
     }
     void Update()
     {
-        if (Render_distance == 0f)
+        spawn_location = new Vector3(0f, 0f, offset);
+        if (Render_distance != 0f)
         {
-            spawn_location = new Vector3(0f, 0f, offset);
+            var Base = Instantiate(base_model,spawn_location,base_model.transform.rotation);
+            Base.transform.SetParent(scene_base);
+            offset += _offset;
+            Render_distance++;
         }
     }
 }
