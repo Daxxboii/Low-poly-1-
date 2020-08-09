b4bba833eeb85012d1af2626f90585733923a34f
Daxxboii <64298070+Daxxboii@users.noreply.github.com>
2020-08-09 23:19:19 +0530

    Update

---
+++ Play Store gaem 1/Assets/Scripts/Player/Player_movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    public float units_per_second;
+    private Rigidbody player_rb;
+
+    private void Awake()
+    {
+        player_rb = this.gameObject.transform.GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        player_rb.velocity = new Vector3(0f, 0f, units_per_second);
+    }
+}
